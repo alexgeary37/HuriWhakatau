df93d52a8482c4cf1938bdc84ff2d47ed4e5e1b3
Alex Geary <alexgeary37@gmail.com>
2020-08-14 00:01:07 +1200

    Install semantic-ui-react and add a little bit of it to App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,6 +4,7 @@ import { Comment } from "./Comment";
 import { Comments } from "/imports/api/comments";
 import { CommentForm } from "./CommentForm";
 import { LoginForm } from "./LoginForm";
+import { Sidebar, Segment } from "semantic-ui-react";
 
 const deleteComment = ({ _id }) => Meteor.call("comments.remove", _id);
 
@@ -28,20 +29,33 @@ export const App = () => {
   }
 
   return (
-    <div className="juryroom">
-      <h1>JuryRoom</h1>
+    <div>
+      {/* 
+      
+      This is attempting to use the semantic-ui-react library to emulate original JuryRoom.
 
-      <div className="comments-and-form">
-        <ul className="comments">
-          {comments.reverse().map((comment) => (
-            <Comment
-              key={comment._id}
-              comment={comment}
-              onDeleteClick={deleteComment}
-            />
-          ))}
-        </ul>
-        <CommentForm />
+      <Sidebar animation="overlay" direction="right">
+        <h1>Verdicts</h1>
+        <button>Propose Verdict</button>
+      </Sidebar>
+      <Sidebar animation="overlay" direction="left">
+        <h1>Title Section</h1>
+        <button>Button to look at</button>
+      </Sidebar> */}
+      <div className="juryroom">
+        <h1>JuryRoom</h1>
+        <div className="comments-and-form">
+          <ul className="comments">
+            {comments.reverse().map((comment) => (
+              <Comment
+                key={comment._id}
+                comment={comment}
+                onDeleteClick={deleteComment}
+              />
+            ))}
+          </ul>
+          <CommentForm />
+        </div>
       </div>
     </div>
   );
