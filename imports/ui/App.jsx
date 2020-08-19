cdf912f1edec0a88afd320500d443aa08dc25d77
Alex Geary <alexgeary37@gmail.com>
2020-08-19 22:31:53 +1200

    Update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import history from "history";
 import { Menu, Container, Dropdown, Icon } from "semantic-ui-react";
 import { Dashboard } from "./Dashboard";
-import { BrowseGroups } from "./BrowseGroups";
+//import { BrowseGroups } from "./BrowseGroups";
 import { Discussion } from "./Discussion";
 
 // Should this be a constant?????????????????????????????????????????????????????/
@@ -13,7 +13,7 @@ const browserHistory = history.createBrowserHistory();
 export const App = () => {
   return (
     <Router history={browserHistory}>
-      <Menu fixed="top" inverted>
+        {/*<Menu fixed="top" inverted>
         <Container className="content-width">
           <Menu.Item as={Link} to="/" header>
             <Icon size="big" name="balance scale" />
@@ -25,10 +25,10 @@ export const App = () => {
             </Dropdown.Menu>
           </Dropdown>
         </Container>
-      </Menu>
+        </Menu>*/}
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        <Route exact path="/groups" component={BrowseGroups} />
+        {/*<Route exact path="/groups" component={BrowseGroups} />*/}
         <Route exact path="/:discussionId" component={Discussion} />
       </Switch>
     </Router>
