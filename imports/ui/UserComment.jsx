ea5774bd294572bd2b3ba23080a2eabd8796b197
Darcy Cowan <dsten32@gmail.com>
2020-08-28 20:45:29 +1200

    refactor comments.

--- imports/ui/UserComment.jsx
+++ imports/ui/UserComment.jsx
@@ -5,35 +5,36 @@ import { Button, Comment } from "semantic-ui-react";
 
 export const UserComment = ({ comment, onSubmitEditClick, onEditClick }) => {
     const [isEditing, setIsEditing] = useState(false);
-  let classes = classnames("comment");
-  let colour = "#F2F2F2";
-  let borderCol = "#E5E5E5";
-
+  // let classes = classnames("comment");
+  // let colour = "#F2F2F2";
+  // let borderCol = "#E5E5E5";
+  let isAuthor = Meteor.userId() === comment.authorId;
   //if user is author then the page renders once for each comment in the discussion.
   // console.log(Meteor.userId());
-  if (Meteor.userId() === comment.authorId) {
-    classes = classnames("comment usersComment");
-    colour = "#EDE8FF";
-    borderCol = "#DFDBF0";
-  }
+  // if (isAuthor) {
+    // classes = classnames("comment usersComment");
+    // colour = "#EDE8FF";
+    // borderCol = "#DFDBF0";
+  // }
 
   // useTracker makes sure the component will re-render when the data changes.
-  const { user } = useTracker(() => {
-    Meteor.subscribe("users");
+    const { user } = useTracker(() => {
+      Meteor.subscribe("users");
 
     return {
       user: Meteor.users.findOne({ _id: comment.authorId }),
     };
   });
+
   return (
     <Comment
       style={{
         margin: 20,
-        backgroundColor: colour,
+        backgroundColor: isAuthor ? "#EDE8FF" : "#F2F2F2",
         borderRadius: 5,
         border: "solid",
         borderWidth: 0.5,
-        borderColor: borderCol,
+        borderColor: isAuthor ? "#DFDBF0" : "#DFDBF0",
         padding: 5,
       }}
     >
@@ -42,9 +43,9 @@ export const UserComment = ({ comment, onSubmitEditClick, onEditClick }) => {
         <Comment.Metadata>
           <div>{comment.postedTime.toDateString()}</div>
         </Comment.Metadata>
-        <Comment.Text id={comment._id + ":text"}>{comment.text}</Comment.Text>
+        <Comment.Text id={comment._id + ":text"} >{comment.text}</Comment.Text>
       </Comment.Content>
-        {Meteor.userId() === comment.authorId ?
+        {isAuthor ?
         <Comment.Actions>
         <Button color='blue' content='Edit' size='mini' active={!isEditing} disabled={isEditing}
         onClick={() => {
