bd34c15bbb49e0ec45f64a24457b0a33e25682a4
Darcy Cowan <dsten32@gmail.com>
2020-09-19 17:20:24 +1200

    added ability to change username and password on user settings page

--- imports/ui/users/UserSettings.jsx
+++ imports/ui/users/UserSettings.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import "/imports/api/security";
+import React, {useState, useEffect} from "react";
 import {useTracker} from "meteor/react-meteor-data";
 import {
     Input,
@@ -18,6 +19,31 @@ import {
 import {NavBar} from "/imports/ui/navigation/NavBar";
 
 export const UserSettings = () => {
+    const [username, setUsername] = useState("OpenlyOctopus");
+    const handleUsername = () => {
+        setUsername(user.username);
+    };
+    const [changeUsername, setChangeUsername] = useState(false);
+    const [changeUserPassword, setChangeUserPassword] = useState(false);
+
+    const [userNewPassword, setUserNewPassword] = useState("");
+    const [userOldPassword, setUserOldPassword] = useState(null);
+    const [isIndigenous, setIsIndigenous] = useState(false);
+    const [userMountain, setUserMountain] = useState("");
+    const [userRiver, setUserRiver] = useState("");
+    const [userWaka, setUserWaka] = useState("");
+    const [userIwi, setUserIwi] = useState("");
+    const [err, setErr] = useState("");
+    //get user participant role status and update variable with call back.
+    // possibly this should be a Promise?
+    Meteor.call("security.hasRole", Meteor.userId(), "PARTICIPANT_I", (error, result) => {
+        if (error) {
+            console.log(error.reason);
+            return;
+        }
+        setIsIndigenous(result);
+    });
+
     // useTracker makes sure the component will re-render when the data changes.
     const {user} = useTracker(() => {
         Meteor.subscribe("users");
@@ -26,9 +52,18 @@ export const UserSettings = () => {
         };
     });
 
-    const [userMountain, setUserMountain] = useState("");
-
-    console.log("mountain: ", userMountain);
+    const updateUsername = () => {
+        Accounts.setUsername(Meteor.userId(), username);
+        // Meteor.call("security.changeUsername", username);
+    };
+    const updateUserPassword = () => {
+        if(userNewPassword.length < 8){
+            setErr("Password must have at least 8 characters")
+        } else {
+            Accounts.changePassword(userOldPassword, userNewPassword);
+            setChangeUserPassword(false);
+        }
+    }
 
     return (
         <div>
@@ -44,31 +79,80 @@ export const UserSettings = () => {
                 <Grid  columns={2}>
                     <GridColumn width={9}>
                         <Card>
-                            <CardHeader as="h4">
-                                Account Details
-                            </CardHeader>
+                            <Card.Content header="Account Details"/>
                             <CardContent>
                                 <Form>
+
                                     <Input labelPosition="left"
                                            type="text"
-                                           value={user && user.username}
-                                           readOnly
+                                           value={user && username}
+                                           readOnly={!changeUsername}
                                            size="mini"
-                                           style={{width:"45%"}}>
+                                           style={{width:"45%"}}
+                                           onInput={({target}) => setUsername(target.value)}>
                                         <Label>Username</Label>
-                                           <input />
-                                    <Button size="mini" content="Change"/>
+                                        <input />
+                                        {!changeUsername ? (
+                                            <Button size="mini" content="Change" onClick={() => {setChangeUsername(true)}}/>
+                                            ) : (
+                                                < Button size="mini" content="Save" onClick={ () => {
+                                            setChangeUsername(false);
+                                            updateUsername();
+                                        }}/>
+                                        )
+                                        }
                                     </Input>
+                                    <br/>
+                                    <br/>
+                                {/*    change password stuff   */}
 
+                                    <Input labelPosition="left"
+                                           type="password"
+                                           value={userOldPassword}
+                                           readOnly={!changeUserPassword}
+                                           size="mini"
+                                           style={{width:"45%"}}
+                                           onInput={({target}) => setUserOldPassword(target.value)}>
+                                        <Label>Password</Label>
+                                        <input />
+                                        {!changeUserPassword &&
+                                            <Button size="mini" content="Change" onClick={() => {
+                                                setChangeUserPassword(true);
+                                                setUserOldPassword("");}}/>
+                                        }
+                                    </Input>
+                                    <br/>
+                                    <br/>
+                                    {changeUserPassword && <div>
+                                    <Form.Input labelPosition="left"
+                                           type="password"
+                                           value={userNewPassword}
+                                           // readOnly={!changeUserPassword}
+                                           error={userNewPassword.length < 8}
+                                           size="mini"
+                                           style={{width: "45%"}}
+                                           onInput={({target}) => setUserNewPassword(target.value)}>
+                                        <Label>New Password</Label>
+                                        <input/>
+                                            <Form.Button size="mini" content="Save" onClick={() => {
+                                                // setChangeUserPassword(false);
 
+                                                updateUserPassword();
+                                            }}/>
+
+                                    </Form.Input>
+                                    {err ? (
+                                        <div style={{ height: "10px", color: "red" }}>{err}</div>
+                                        ) : (
+                                        <div style={{ height: "10px" }} />
+                                        )}
+                                        </div>
+                                    }
                                 </Form>
-                                content
                             </CardContent>
                         </Card>
                         <Card>
-                            <CardHeader>
-                                some other stuff
-                            </CardHeader>
+                            <Card.Content header="stuff"/>
                             <CardContent>
 
 
@@ -79,74 +163,74 @@ export const UserSettings = () => {
 
                     {/*</GridColumn>*/}
                     <GridColumn width={7}>
+                        {!isIndigenous &&
                         <Card>
-                        <CardHeader>
-                            Pepeha
-                        </CardHeader>
+                            <Card.Content header="Pepeha"/>
                             <CardContent>
                                 <Form>
                                     <Input value={userMountain}
                                            labelPosition="left"
                                            type="text"
                                            size="mini"
-                                           style={{width:"45%"}}
+                                           style={{width: "45%"}}
                                            onInput={({target}) => setUserMountain(target.value)}
                                     >
-                                    <Label style={{width:"55%"}}>Mountain</Label>
-                                        <input />
-                                        <Button icon style={{marginLeft:"20px"}}>
-                                            <Icon className="mountain" />
+                                        <Label style={{width: "55%"}}>Mountain</Label>
+                                        <input/>
+                                        <Button icon style={{marginLeft: "20px"}}>
+                                            <Icon className="mountain"/>
                                         </Button>
                                     </Input>
                                     <br/>
                                     <br/>
-                                    <Input value={userMountain}
-                                                   labelPosition="left"
-                                                   type="text"
-                                                   size="mini"
-                                                   style={{width:"45%"}}
-                                                   onInput={({target}) => setUserMountain(target.value)}
-                                >
-                                    <Label style={{width:"55%"}}>River</Label>
-                                    <input />
-                                    <Button icon style={{marginLeft:"20px"}}>
-                                        <Icon className="river" />
-                                    </Button>
-                                </Input>
-                                    <br/>
-                                    <br/>
-                                    <Input value={userMountain}
+                                    <Input value={userRiver}
                                            labelPosition="left"
                                            type="text"
                                            size="mini"
-                                           style={{width:"45%"}}
-                                           onInput={({target}) => setUserMountain(target.value)}
+                                           style={{width: "45%"}}
+                                           onInput={({target}) => setUserRiver(target.value)}
                                     >
-                                        <Label style={{width:"55%"}}>Waka</Label>
-                                        <input />
-                                        <Button icon style={{marginLeft:"20px"}}>
-                                            <Icon className="waka" />
+                                        <Label style={{width: "55%"}}>River</Label>
+                                        <input/>
+                                        <Button icon style={{marginLeft: "20px"}}>
+                                            <Icon className="river"/>
                                         </Button>
                                     </Input>
                                     <br/>
                                     <br/>
-                                    <Input value={userMountain}
+                                    <Input value={userWaka}
                                            labelPosition="left"
                                            type="text"
                                            size="mini"
-                                           style={{width:"45%"}}
-                                           onInput={({target}) => setUserMountain(target.value)}
+                                           style={{width: "45%"}}
+                                           onInput={({target}) => setUserWaka(target.value)}
                                     >
-                                        <Label style={{width:"55%"}}>Iwi</Label>
-                                        <input />
-                                        <Button icon style={{marginLeft:"20px"}}>
-                                            <Icon className="iwi" />
+                                        <Label style={{width: "55%"}}>Waka</Label>
+                                        <input/>
+                                        <Button icon style={{marginLeft: "20px"}}>
+                                            <Icon className="waka"/>
+                                        </Button>
+                                    </Input>
+                                    <br/>
+                                    <br/>
+                                    <Input value={userIwi}
+                                           labelPosition="left"
+                                           type="text"
+                                           size="mini"
+                                           style={{width: "45%"}}
+                                           onInput={({target}) => setUserIwi(target.value)}
+                                    >
+                                        <Label style={{width: "55%"}}>Iwi</Label>
+                                        <input/>
+                                        <Button icon style={{marginLeft: "20px"}}>
+                                            <Icon className="iwi"/>
                                         </Button>
                                     </Input>
 
                                 </Form>
                             </CardContent>
                         </Card>
+                        }
                     </GridColumn>
                 </Grid>
             </Container>
