c5136005e7eb9ada5ca1092214b3745bf1fd2f01
Darcy Cowan <dsten32@gmail.com>
2020-11-29 13:03:54 +1300

    fixed rating component, created api mongodb queries to get a random experiment and comment or random personality question. todo: what happens when each is saved? comment ratings might get own collection while personality questions get added to user.

--- imports/api/experiments.js
+++ imports/api/experiments.js
@@ -1,132 +1,145 @@
-import { Mongo } from "meteor/mongo";
-import { check } from "meteor/check";
-import { ScenarioSets } from "./scenarioSets";
-import { Scenarios } from "./scenarios";
-import { DiscussionTemplates } from "./discussionTemplate";
-import { Discussions } from "./discussions";
+import {Mongo} from "meteor/mongo";
+import {check} from "meteor/check";
+import {ScenarioSets} from "./scenarioSets";
+import {Scenarios} from "./scenarios";
+import {DiscussionTemplates} from "./discussionTemplate";
+import {Discussions} from "./discussions";
+import awaitAsyncGenerator from "@babel/runtime/helpers/esm/awaitAsyncGenerator";
 
 export const Experiments = new Mongo.Collection("experiments");
 
 Meteor.methods({
-  // Insert a category into the category collection in the db.
-  // name: the category name
-  // Called from *****
-  "experiments.create"(
-    name,
-    description,
-    groupId,
-    scenarioSetId,
-    hasIntroduction,
-    ratings,
-  ) {
-    check(name, String);
-    check(description, String);
-    check(groupId, String);
-    check(scenarioSetId, String);
-    check(ratings, Array);
-    let discussionIds = [];
-    console.log(ratings);
-    //addcheck for user admin/researcher role
-
-    const experimentId = Experiments.insert({
-      name: name,
-      description: description,
-      groupId: groupId,
-      scenarioSetId: scenarioSetId,
-      ratings: ratings,
-      createdAt: new Date(),
-      createdBy: Meteor.userId(),
-    });
-
-    console.log(experimentId);
-
-    //create intro discussion if needed
-    if (hasIntroduction) {
-      console.log("creating introduction");
-      const introId = Meteor.call(
-        "discussions.insertIntroduction",
-        "wWtYSX9zP7b5yeNo7",
+    // Insert a category into the category collection in the db.
+    // name: the category name
+    // Called from *****
+    "experiments.create"(
+        name,
+        description,
         groupId,
-        0
-      );
-      console.log("adding intro id to exp ", introId);
-      Experiments.update(experimentId, {
-        $push: { discussions: introId },
-      });
-      console.log("adding intro id to set", introId);
-      discussionIds.push(introId);
-      console.log(discussionIds);
-    }
+        scenarioSetId,
+        hasIntroduction,
+        ratings,
+    ) {
+        check(name, String);
+        check(description, String);
+        check(groupId, String);
+        check(scenarioSetId, String);
+        check(ratings, Array);
+        let discussionIds = [];
+        console.log(ratings);
+        //addcheck for user admin/researcher role
 
-    const set = ScenarioSets.findOne({ _id: scenarioSetId });
-    const scenarios = Scenarios.find({ _id: { $in: set.scenarios } }).fetch();
-    //for each scenario get discussion time limit and add to discussion
-    for (i = 0; i < scenarios.length; i++) {
-      console.log("creating discussion");
-      const discussionTemplate = DiscussionTemplates.findOne({
-        _id: scenarios[i].discussionTemplateId,
-      });
-      console.log("inserting discussion", discussionTemplate);
-      const discussionId = Meteor.call(
-        "discussions.insert",
-        scenarios[i]._id,
-        groupId,
-        discussionTemplate.timeLimit,
-        discussionTemplate.isHui,
-        discussionTemplate.isPublic,
-      );
-      console.log("adding discussion id to exp", discussionId);
-      Experiments.update(experimentId, {
-        $push: { discussions: discussionId },
-      });
-      console.log("adding disc id to set", discussionId);
-      discussionIds.push(discussionId);
-      console.log(discussionIds);
-    }
-    console.log(discussionIds);
-    // to each discussion add the id for the next the discussion
-    for (let id = 0; id < discussionIds.length - 1; id++) {
-      console.log(
-        "adding nextDiscussionId",
-        discussionIds[id],
-        " -> ",
-        discussionIds[id + 1]
-      );
-      Discussions.update(discussionIds[id], {
-        $set: { nextDiscussion: discussionIds[id + 1] },
-      });
-    }
-  },
+        const experimentId = Experiments.insert({
+            name: name,
+            description: description,
+            groupId: groupId,
+            scenarioSetId: scenarioSetId,
+            ratings: ratings,
+            createdAt: new Date(),
+            createdBy: Meteor.userId(),
+        });
 
-  // Remove an experiment from the experiments collection in the db.
-  // experimentId: _id of the comment to be removed
-  //
-  "experiments.remove"(experimentId) {
-    check(experimentId, String);
-    //add role check
+        console.log(experimentId);
 
-    Experiments.remove(experimentId);
-  },
+        //create intro discussion if needed
+        if (hasIntroduction) {
+            console.log("creating introduction");
+            const introId = Meteor.call(
+                "discussions.insertIntroduction",
+                "wWtYSX9zP7b5yeNo7",
+                groupId,
+                0
+            );
+            console.log("adding intro id to exp ", introId);
+            Experiments.update(experimentId, {
+                $push: {discussions: introId},
+            });
+            console.log("adding intro id to set", introId);
+            discussionIds.push(introId);
+            console.log(discussionIds);
+        }
+
+        const set = ScenarioSets.findOne({_id: scenarioSetId});
+        const scenarios = Scenarios.find({_id: {$in: set.scenarios}}).fetch();
+        //for each scenario get discussion time limit and add to discussion
+        for (i = 0; i < scenarios.length; i++) {
+            console.log("creating discussion");
+            const discussionTemplate = DiscussionTemplates.findOne({
+                _id: scenarios[i].discussionTemplateId,
+            });
+            console.log("inserting discussion", discussionTemplate);
+            const discussionId = Meteor.call(
+                "discussions.insert",
+                scenarios[i]._id,
+                groupId,
+                discussionTemplate.timeLimit,
+                discussionTemplate.isHui,
+                discussionTemplate.isPublic,
+            );
+            console.log("adding discussion id to exp", discussionId);
+            Experiments.update(experimentId, {
+                $push: {discussions: discussionId},
+            });
+            console.log("adding disc id to set", discussionId);
+            discussionIds.push(discussionId);
+            console.log(discussionIds);
+        }
+        console.log(discussionIds);
+        // to each discussion add the id for the next the discussion
+        for (let id = 0; id < discussionIds.length - 1; id++) {
+            console.log(
+                "adding nextDiscussionId",
+                discussionIds[id],
+                " -> ",
+                discussionIds[id + 1]
+            );
+            Discussions.update(discussionIds[id], {
+                $set: {nextDiscussion: discussionIds[id + 1]},
+            });
+        }
+    },
+
+    //get a random experiment with a non-empty rating
+    "experiments.getRandomExperimentForRating"() {
+        console.log("random experiment query triggered");
+        const fetchedExp = Experiments.rawCollection().aggregate([
+            {$match: {ratings: {$elemMatch: {rating: {$ne: ""}}}}},
+            {$sample: {size: 1}}
+        ])
+            .toArray();
+        console.log(fetchedExp);
+        return fetchedExp;
+    },
+
+    // Remove an experiment from the experiments collection in the db.
+    // experimentId: _id of the comment to be removed
+    //
+    "experiments.remove"(experimentId) {
+        check(experimentId, String);
+        //add role check
+
+        Experiments.remove(experimentId);
+    },
 });
 
 if (Meteor.isServer) {
-  // Experiments.remove({});
+    // Experiments.remove({});
 
-  Meteor.publish("experiments", function () {
-    return Experiments.find(
-      {},
-      {
-        fields: {
-          name: 1,
-          description: 1,
-          groupId: 1,
-          scenarioSetId: 1,
-          ratings: 1,
-          discussions: 1,
-          createdAt: 1,
-          createdBy: 1,
-        },
-      }
-    );
-  });
+    Meteor.publish("experiments", function () {
+        return Experiments.find(
+            {},
+            {
+                fields: {
+                    name: 1,
+                    description: 1,
+                    groupId: 1,
+                    scenarioSetId: 1,
+                    ratings: 1,
+                    discussions: 1,
+                    createdAt: 1,
+                    createdBy: 1,
+                },
+            }
+        );
+    });
 }
--- imports/ui/navigation/RatingComponent.jsx
+++ imports/ui/navigation/RatingComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Random } from 'meteor/random'
+import React, {useEffect, useState} from "react";
+import {Random} from 'meteor/random'
 import {indices, responseSet} from '/imports/api/ratingResponses'
 import {useTracker} from "meteor/react-meteor-data";
 import {
@@ -8,87 +8,51 @@ import {
     Icon, Card, ListItem, Rating,
     Divider, Image, Sidebar, Tab, ListContent, Checkbox,
 } from "semantic-ui-react";
-import {Comments} from "/imports/api/comments";
-import {Experiments} from "../../api/experiments";
-import {Personality} from "../../api/personality";
-import Cookies from "universal-cookie/cjs/Cookies";
 
 export const RatingComponent = () => {
     const [answerInt, setAnswerInt] = useState(0);
+    const [typeOfQuestion, setTypeOfQuestion] = useState(-1);
     const [answerString, setAnswerString] = useState("");
-
-    /*
-    * intent:
-    * if comment rating:
-    * retrieve ratings and discussion ids from random experiment where experiment.ratings[0].rating != ""
-    * From ratings array randomly choose one where rating != ""
-    * From discussions array in experiment choose random id.
-    * Retrieve random comment where discussion id matches randomly chosen id.
-    * Retrieve ratings set where type === ratings.responseType
-    *
-    *
-    * if personality question rating:
-    * get random personality question from random personality quiz document
-    *
-    * get the indices for the response set for the scale set & generate rating range label set from the ratings
-    * set and the indices (eg scale = 2, responseSetIndices = indices[scale]
-    * for responseSetIndices labelSet.push(responseSet[index]))
-    *
-    * questionItem object = {
-    *   itemId: comment ? comment._id : question._id,
-    *   itemNumber: comment ? 0 : question.item,
-    *   headerText: comment ? rating.rating : question.text,
-    *   bodyText: comment ? comment.text : "",
-    *   scale: rating.scale,
-    *   reverse: rating.reverse,
-    *   ratingLabels: labelSet,
-    * }
-    * */
-    // cut down personality question for testing. should be in a collection and retrieved randomly.
-
-    const {questionItem} = useTracker(() => {
-        const [collection, type] = Random.choice([[Experiments, 1], [Personality, 0]]);
-        const collectionSub = type ? Meteor.subscribe("experiments") : Meteor.subscribe("personality");
-        // item variables. may be condensed / modified later.
-        let rating;
-        let responseIndices;
-        let responses;
-        let labelSet = [];
-        let comment;
-        let question;
-        let id="";
-        let itemNum=0;
-        let header="";
-        let body="";
-        let ratingScale=7;
-        let ratingReverse=false;
-        if(collectionSub.ready()){
-            if(type){
-                // get experiment,TODO: change this so it's while random experiment.ratings.rating === "" get random
-                // experiment. Like the random username rather than the one experiment I know works.
-                // const experiment = collection.findOne({_id: "aEwAZuSd32fzaWYWz"});
-                
-                //const experiment = collection.findOne({ $filter: { input: ratings, as: ratings, cond: {} } });
-                const experiments = collection.find({}).fetch();
-                let experiment;
-                for (i = 0; i < experiments.length; i += 1) {
-                  ex = experiments[i];
-                  for (j = 0; j < ex.ratings.length; j += 1) {
-                    if (ex.ratings[j].rating !== "") {
-                      experiment = ex;
-                      break;
-                    }
-                  }
+    const [submitErr, setSubmitErr] = useState("");
+    const [questionItem, setQuestionItem] = useState(
+        {
+                    itemId: "",
+                    itemNumber: 0,
+                    headerText: "",
+                    bodyText: "",
+                    scale: 7,
+                    reverse: false,
+                    ratingLabels: [],
                 }
-                
-                // Make sure experiment is defined/assigned in the above loop. If none were found and it wasn't assigned, what happens?? Will the next if condition have to be changed?
-                console.log("experiment", experiment);
-                
-                if(experiment.ratings) {
-                    // for the moment get number of ratings the at != "",
-                    // todo change the submit experiment so ratings === "" are not submitted.
+    );
 
-                    let exptRatings = experiment.ratings.filter( rating => rating.rating != "");
+    let rating;
+    let responseIndices;
+    let responses;
+    let labelSet = [];
+    let comment;
+    let question;
+    let id = "";
+    let itemNum = 0;
+    let header = "";
+    let body = "";
+    let ratingScale = 7;
+    let ratingReverse = false;
+    let questionType;
+
+    const updateQuestion = () =>{
+        setSubmitErr("");
+        setAnswerInt(0);
+        setAnswerString("");
+        questionType = Random.choice([0,1]);
+        setTypeOfQuestion(questionType);
+        if (questionType) {
+            Meteor.call("experiments.getRandomExperimentForRating"
+                , (err, fetchedExperimentArray) => {
+                    // console.log(expy[0]);
+                    let experiment = fetchedExperimentArray[0];
+                    let exptRatings = experiment.ratings.filter(rating => rating.rating != "");
+                    console.log(exptRatings);
                     // Select a random rating from the experiment's rating set
                     rating = exptRatings[Math.floor(Math.random() * Math.floor(exptRatings.length))]
                     // find the appropriate response set
@@ -100,93 +64,117 @@ export const RatingComponent = () => {
                     // responseIndices.forEach(index => labelSet.push(responses.fullRange[index]));
                     indices[ratingScale].forEach(index => labelSet.push(responses.fullRange[index]));
                     header = rating.rating;
-                    
-                    // get comment
-                    let commentSub = Meteor.subscribe("comments");
-                    if (commentSub.ready()) {
-                        // get a random comment for a discussion in the experiment.
-                        // comment = Comments.find({discussionId: {$in: experiment.discussions}}).fetch();
-                        comment = Comments.findOne({_id:"bkXE3GtYtkxW2q6Zt"});
-                        id = comment._id;
-                        body = comment.text;
-                    }
-                }
-            } else {
-                const personalityQuestionnaire = collection.findOne({_id:"eEg6qCkMPsBq53khD"});
-                if(personalityQuestionnaire.items) {
-                    question = personalityQuestionnaire.items[Math.floor(
-                        Math.random() * Math.floor(personalityQuestionnaire.items.length))];
-                    // question = personalityQ;
-                    itemNum = question.item;
-                    header = question.text;
-                    body = "";
-                    ratingScale = question.scale;
-                    ratingReverse = question.scoringReversed;
-                    responses = responseSet.find(set => set.responseType === question.responseType)
-                    responseIndices = indices[ratingScale];
-                    // get the appropriate labels given the number of responses set eg 2, 5 or 7
-                    responseIndices.forEach(index => labelSet.push(responses.fullRange[index]));
-                }
-            }
+                    // get a random comment for a discussion in the experiment.
+                    Meteor.call("comments.getRandomExperimentCommentForRating", experiment.discussions
+                        , (err, fetchedCommentArray) => {
+                            comment = fetchedCommentArray[0];
+                            // Comments.findOne({_id: "bkXE3GtYtkxW2q6Zt"});
+                            id = comment._id;
+                            body = comment.text;
+
+                            setQuestionItem({
+                                itemId: id,
+                                itemNumber: itemNum,
+                                headerText: header,
+                                bodyText: body,
+                                scale: ratingScale,
+                                reverse: ratingReverse,
+                                ratingLabels: labelSet,
+                            })
+                        });
+                    });
+        } else {
+            Meteor.call("personality.getRandomQuestion", (err, fetchedQuestionArray) => {
+                question = fetchedQuestionArray[0];
+                console.log(question);
+                id = question._id;
+                itemNum = question.item.item;
+                header = question.item.text;
+                body = "";
+                ratingScale = question.item.scale;
+                ratingReverse = question.item.scoringReversed;
+                responses = responseSet.find(set => set.responseType === question.item.responseType)
+                responseIndices = indices[ratingScale];
+                // get the appropriate labels given the number of responses set eg 2, 5 or 7
+                responseIndices.forEach(index => labelSet.push(responses.fullRange[index]));
+
+                setQuestionItem({
+                    itemId: id,
+                    itemNumber: itemNum,
+                    headerText: header,
+                    bodyText: body,
+                    scale: ratingScale,
+                    reverse: ratingReverse,
+                    ratingLabels: labelSet,
+                })
+            })
         }
+    }
 
-        return {
-            questionItem:{
-                itemId: id,
-                itemNumber: itemNum,
-                headerText: header,
-                bodyText: body,
-                scale: ratingScale,
-                reverse: ratingReverse,
-                ratingLabels: labelSet,
-            }
-        };
-    })
+    // fetch first question on page load
+    useEffect(() => {
+        updateQuestion();
+    }, [])
 
-    // todo work out what submitting an answer looks like / does.
     const addAnswerValue = (value) => {
-        // if reverse coding is active.
-        // score = ratingLabels.indexOf(value) if reverse is false, else ratingLabels.reverse().indexOf(value).
+        let ratingLabels = [...questionItem.ratingLabels];
+        let score = questionItem.reverse ?
+            ratingLabels.reverse().indexOf(value) : ratingLabels.indexOf(value)
 
         setAnswerString(value);
-        setAnswerInt(item.range.indexOf(value)),
+        setAnswerInt(score);
         console.log(answerString, answerInt);
     }
 
     const submitAnswer = () => {
-
+        //todo work out what submitting an answer looks like / does.
+        if(answerString){
+            console.log("question type was: ", typeOfQuestion);
+            if(typeOfQuestion){
+                console.log("a comment rating answer")
+            } else {
+                console.log("a personality question answer")
+            }
+            updateQuestion();
+        } else {
+            setSubmitErr("You must select an option before submitting")
+        }
     }
 
     return (
         <div>
             <Header as={'h3'} content={"question"} inverted/>
-            <ListItem key={'akey'}>
+            <ListItem key={Math.random()}>
                 <ListContent as={Segment} style={{
                     backgroundColor: "#c4c4c4",
                 }}>
-                    <List.Header as={'h4'} content={questionItem && questionItem.headerText} />
+                    <List.Header as={'h4'}
+                                 content={questionItem && questionItem.headerText}
+                                 // content={aStub && aStub.headerText}
+                    />
                     {questionItem.bodyText &&
                     <List.Description as={Segment}
                                       content={questionItem && questionItem.bodyText}
                     />
                     }
                     <hr/>
-                        {/*<input type="range" min="0" max={item.scale - 1} value={answer} name={ratingInfo.type} id={item._id + "#" + item.item}*/}
-                        {/*       onChange={({target}) => addAnswerValue(target.value)}/>*/}
                     {questionItem.ratingLabels.map((label) =>
                         <Form.Field>
-                        <Checkbox
-                        radio
-                        label={label}
-                        name='quixbox'
-                        value={label}
-                        checked={answerString === label}
-                        // onChange={({target}) => addAnswerValue(target.value)}
-                         onChange={(e, data) => addAnswerValue(data.value)}
-                        />
+                            <Checkbox
+                                key={label}
+                                radio
+                                label={label}
+                                name='quixbox'
+                                value={label}
+                                checked={answerString === label}
+                                // onChange={({target}) => addAnswerValue(target.value)}
+                                onChange={(e, data) => addAnswerValue(data.value)}
+                            />
                         </Form.Field>
                     )}
+                    <p style={{color:'red', height:"10px"}}>{submitErr && submitErr}</p>
                     <Button content={'Submit Answer'} onClick={submitAnswer}/>
+                    <Button content={'Different Question'} onClick={updateQuestion}/>
                 </ListContent>
             </ListItem>
         </div>
